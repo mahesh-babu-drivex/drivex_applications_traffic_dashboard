deb26a2b20c231e141aa0128f5e13a9c279ba130
Drivex Admin <admin@drivex.in>
2024-05-14 08:30:14 +0000

    added buy and sell

--- pages/Page1/jsobjects/JSObject2/JSObject2.js
+++ pages/Page1/jsobjects/JSObject2/JSObject2.js
@@ -1,14 +1,135 @@
 export default {
-    autoNavigate: () => {
-        let intervalId;
+  fetchData() {
+    const inputData = last7sales.data;
 
-        const navigateEvery30Minutes = () => {
-            navigateTo('Page2'); // Replace 'Page2' with the actual page name or URL
-        };
+    const dates = [];
+    const testride = [];
+    const whatsapp = [];
+		const enquiry = [];
 
-        intervalId = setInterval(navigateEvery30Minutes, 30 * 60 * 1000);
+    // Populate arrays with data
+    inputData.forEach((item) => {
+      dates.push(item.date);
+      testride.push(Number(item.TEST_RIDE));
+      whatsapp.push(Number(item.WHATSAPP_TOUR));
+			enquiry.push(Number(item.ENQUIRY));
 
-        // If you want to stop the automatic navigation, you can use clearInterval like this:
-        // clearInterval(intervalId);
+    });
+
+    const transformedData = {
+      dates: dates,
+      test_ride: testride,
+      whats_app: whatsapp,
+			enq:enquiry
+    };
+
+    return transformedData;
+  },
+	option : {
+  title: {
+    text: 'Last Seven Days Trend',
+     left: 'center', // Center the title horizontally
+    top: '0%'
+
+  },
+  tooltip: {
+    trigger: 'axis'
+  },
+  legend: {
+    data: ['Test Ride','Whatsapp Tour','Enquiry'],
+		 orient: 'vertical', // Set the orientation to vertical
+    right: '5%',        // Adjust the right distance as needed
+    top: 'top'
+  },
+  grid: {
+    left: '3%',
+    right: '9%',
+    bottom: '7%',
+    containLabel: true
+  },
+  toolbox: {
+    feature: {
+      saveAsImage: {}
     }
-};
+  },
+  xAxis: {
+    type: 'category',
+    boundaryGap: true,
+		name:'Date',
+		nameTextStyle: {
+      fontSize: 20, // Adjust the font size for the x-axis title
+      fontWeight: 'bold' // Make the x-axis title bold
+    },
+    data: this.fetchData().dates,
+				axisLabel: {
+      show: true,
+       position: 'top' ,
+				fontSize:16,
+				      fontWeight: 'bold' 
+// You can adjust the position as needed
+   }
+  },
+  yAxis: {
+    type: 'value',
+		name:'Count',
+		nameTextStyle: {
+      fontSize: 20, // Adjust the font size for the x-axis title
+      fontWeight: 'bold' // Make the x-axis title bold
+    },
+				axisLabel: {
+      show: true,
+				fontSize:16,
+				      fontWeight: 'bold' ,
+					      position: 'insideLeft'// Place the title inside the left of the axis labels
+
+// You can adjust the position as needed
+   }
+		 // axisLabel: {
+      // show: true // Set to true to display labels on the axis
+      // // You can add more formatting options here if needed
+    // }
+		
+  },
+  series: [
+    {
+      name: 'Test Ride',
+      type: 'line',
+      // stack: 'Total',
+      data: this.fetchData().test_ride,
+					label: {
+      show: true,
+       position: 'top' ,
+				fontSize:16,
+				      fontWeight: 'bold' 
+// You can adjust the position as needed
+   }
+    },
+    {
+      name: 'Whatsapp Tour',
+      type: 'line',
+      // stack: 'Total',
+      data: this.fetchData().whats_app,
+			label: {
+      show: true,
+       position: 'top' ,
+				fontSize:16,
+				      fontWeight: 'bold' 
+// You can adjust the position as needed
+   }
+    },
+		 {
+      name: 'Enquiry',
+      type: 'line',
+      // stack: 'Total',
+      data: this.fetchData().enq,
+					label: {
+      show: true,
+       position: 'top' ,
+				fontSize:16,
+				      fontWeight: 'bold' 
+// You can adjust the position as needed
+   }
+    }
+  ]
+}
+}
